a6856629ab667ccbbcdcaccfb14a89657170f675
dbartolome <diego@santacc.es>
2026-02-21 19:08:20 +0100

    fix: set github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE_PATH || "/",
+  base: '/tiendaTech/',
   server: {
     host: "localhost",
     port: 8080,
